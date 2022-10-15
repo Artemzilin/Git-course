cf805001a86342e138cc6d64c635662b695d6549
Artem Zhilin <dellayn3@gmail.com>
2022-10-15 22:19:25 +0300

    Learn JavaScript

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 document.body.innerHTML = '<h1>Hello from JavaScript</h1>'
 console.log('Test !!!!!!!!!!!');
+
+
+
+let count = 0;
+
+function cc(card) {
+
+	switch (card) {
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			count++;
+			break;
+
+		case 10:
+		case 'J':
+		case 'Q':
+		case 'K':
+		case 'A':
+			count--;
+	}
+	if (count > 0) {
+		return count + " Bet";
+	} else {
+		return count + " Hold";
+	}
+
+
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
